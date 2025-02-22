a360daf78c0667e26968462cfd31b8a528457d1b
Michal Kucharczyk <kucharczi@gmail.com>
2025-02-22 15:27:19 +0100

    interfaces

--- src/ratatai/assistant.py
+++ src/ratatai/assistant.py
@@ -2,14 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class CookingAssistant(ABC):
-    """Class that should encapsulate the entire cooking assistant functionality.
-
-    Interaction loop w/ the cooking assistant:
-    > user presses [ENTER]
-    > user says something
-    > user presses [ENTER]
-    > assistant generates a response
-    """
+    """Class that should encapsulate the entire cooking assistant functionality."""
 
     @abstractmethod
     def run(self) -> None:
